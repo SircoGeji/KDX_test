1c113376fe6f0be14d38ebe15705817ea5b3ad00
SircoGeji <semenkov777@gmail.com>
2020-09-04 15:27:47 +0300

    Updates

---
+++ precache-manifest.dab94315d87457702b721930c98df86b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "065db8e9898428d8fd1ee902f1c7dc07",
+    "url": "/KDX_test/index.html"
+  },
+  {
+    "revision": "5c3a0a405abb9724188c",
+    "url": "/KDX_test/static/css/main.19142374.chunk.css"
+  },
+  {
+    "revision": "3242d756915bb02ce74c",
+    "url": "/KDX_test/static/js/2.22ea1562.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/KDX_test/static/js/2.22ea1562.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c3a0a405abb9724188c",
+    "url": "/KDX_test/static/js/main.9e0db78d.chunk.js"
+  },
+  {
+    "revision": "7297940ee8b55f219dc7",
+    "url": "/KDX_test/static/js/runtime-main.6faf8c4e.js"
+  },
+  {
+    "revision": "d34825b219fc31a285c34550d88180a4",
+    "url": "/KDX_test/static/media/DUCK 1.d34825b2.png"
+  }
+]);
